bad50456787a2c06bbe2aaafeab05f5d74cbf361
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-06 22:15:47 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -5,5 +5,33 @@ document.onkeydown = function(e){
         setTimeout(()=>{
             dino.classList.remove('animateDino')
         },500);
+    }else if (e.keyCode===39){
+        let dino = document.querySelector('.dino')
+        let dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'))
+        dino.style.left = dinoX+112+"px"
+    }else if (e.keyCode===37){
+        let dino = document.querySelector('.dino')
+        let dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'))
+        dino.style.left = dinoX-112+"px"
     }
 }
+
+setInterval(()=>{
+    let dino = document.querySelector('.dino')
+    let gameOver = document.querySelector('.gameOver')
+    let obstacle = document.querySelector('.obstacle')
+
+    let dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+    let dy = parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
+
+    let ox = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left'));
+    let oy = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top'));
+
+    let offsetX = Math.abs(ox-dx);
+    let offsetY = Math.abs(oy-dy);
+
+    if (offsetX<93 && offsetY<52){
+        gameOver.style.visibility = 'visible';
+        obstacle.classList.remove('obstacleAni')
+    }
+},100)
