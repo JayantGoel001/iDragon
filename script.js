ad705365874ad1476bd4987e08c417a4b3581406
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-06 23:28:35 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let score = 0
 let cross = true
-
+let GameEnded = false
 let audio = new Audio('music.mp3')
 
 setTimeout(()=>{
@@ -8,65 +8,71 @@ setTimeout(()=>{
 },2000)
 
 document.onkeydown = function(e){
-    if (e.keyCode === 38) {
-        let dino = document.querySelector('.dino')
-        dino.classList.add('animateDino')
-        setTimeout(()=>{
-            dino.classList.remove('animateDino')
-        },700);
-    }else if (e.keyCode===39){
-        let dino = document.querySelector('.dino')
-        let dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'))
-        dino.style.left = dinoX+112+"px"
-    }else if (e.keyCode===37){
-        let dino = document.querySelector('.dino')
-        let dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'))
-        dino.style.left = dinoX-112+"px"
+    if (!GameEnded) {
+        if (e.keyCode === 38) {
+            let dino = document.querySelector('.dino')
+            dino.classList.add('animateDino')
+            setTimeout(() => {
+                dino.classList.remove('animateDino')
+            }, 700);
+        } else if (e.keyCode === 39) {
+            let dino = document.querySelector('.dino')
+            let dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'))
+            dino.style.left = dinoX + 112 + "px"
+        } else if (e.keyCode === 37) {
+            let dino = document.querySelector('.dino')
+            let dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'))
+            dino.style.left = dinoX - 112 + "px"
+        }
     }
 }
 
 setInterval(()=>{
-    let dino = document.querySelector('.dino')
-    let gameOver = document.querySelector('.gameOver')
-    let obstacle = document.querySelector('.obstacle')
+    if (!GameEnded) {
+        let dino = document.querySelector('.dino')
+        let gameOver = document.querySelector('.gameOver')
+        let obstacle = document.querySelector('.obstacle')
 
-    let dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    let dy = parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
+        let dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        let dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
 
-    let ox = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left'));
-    let oy = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top'));
+        let ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+        let oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
 
-    let offsetX = Math.abs(ox-dx);
-    let offsetY = Math.abs(oy-dy);
+        let offsetX = Math.abs(ox - dx);
+        let offsetY = Math.abs(oy - dy);
 
-    if (offsetX<73 && offsetY<52){
-        gameOver.innerHTML  = 'GAME OVER';
-        obstacle.classList.remove('obstacleAni')
-        dino.style.visibility = 'hidden';
+        if (offsetX < 73 && offsetY < 52) {
+            GameEnded = true;
+            gameOver.innerHTML = 'GAME OVER';
+            obstacle.classList.remove('obstacleAni')
+            dino.style.visibility = 'hidden';
 
-        let audioGameOver = new Audio('gameOver.mp3')
+            let audioGameOver = new Audio('gameOver.mp3')
 
-        audioGameOver.autoplay = true
-        audioGameOver.play()
-        audio.pause();
-        setTimeout(()=>{
+            audioGameOver.autoplay = true
+            audioGameOver.play()
+            audio.pause();
 
-            audioGameOver.pause()
-        },1000);
-        score-=100;
+            setTimeout(() => {
+                audioGameOver.pause()
+            }, 900);
 
-    }else if (offsetX<145 && cross) {
-        score+=100;
-        updateScore(score);
-        cross=false;
-        setTimeout(()=>{
-            cross = true;
-        },1000);
-        setTimeout(()=>{
-            let aniDur = parseFloat(window.getComputedStyle(obstacle,null).getPropertyValue('animation-duration'))
-            obstacle.style.animationDuration = aniDur-0.1 + 's'
-        },500);
+            score -= 100;
 
+        } else if (offsetX < 145 && cross) {
+            score += 100;
+            updateScore(score);
+            cross = false;
+            setTimeout(() => {
+                cross = true;
+            }, 1000);
+            setTimeout(() => {
+                let aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'))
+                obstacle.style.animationDuration = aniDur - 0.1 + 's'
+            }, 500);
+
+        }
     }
 },10)
 
